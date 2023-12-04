a04096e96392e96c54cff67d5c6909217ef2b7de
gongahkia <angryapplegravy@gmail.com>
2023-12-04 19:29:36 +0800

    finished

--- gong/day-4/day-4.py
+++ gong/day-4/day-4.py
@@ -1,25 +1,39 @@
-# type: ignore
-
 def parser() -> [[str]]:
-    with open("input","r") as fhand:
+    with open("input2","r") as fhand:
         t:[[str]] = [line.strip().split("|") for line in fhand]
     return [(el[0].split(":")[0].lstrip("Card "), el[0].split(":")[1].strip().split(" "), el[1].strip().split(" ")) for el in t]
 
 def sanitise(parsed:[[str]]) -> [[int]]:
-    return [([int(b) for b in el[0]], [int(i) for i in el[1] if i != ""], [int(q) for q in el[2] if q != ""]) for el in parsed]
+    return [([int("".join(el[0]))], [int(i) for i in el[1] if i != ""], [int(q) for q in el[2] if q != ""]) for el in parsed]
 
-def a() -> None:
+def check(win:[int],urs:[int]) -> int:
+    fin:int = 0
+    for val in urs:
+        if val in win:
+            fin += 1
+    return fin
+
+def a() -> int:
     tot:[float] = []
     parsed:[[int]] = sanitise(parser())
     for game in parsed:
         tem:float = 0.5
-        for num in game[2]:
-            if num in game[1]:
-                tem = tem * 2
+        for i in range(check(game[1],game[2])):
+            tem = tem * 2
         if tem != 0.5:
             tot.append(int(tem))
     return sum(tot)
 
-print(a())
+def b() -> int:
+    stor:{str:int} = {}
+    parsed:[[int]] = sanitise(parser())
+    for game in parsed:
+        rep = 1
+        if str(game[0][0]) in stor:
+            rep = stor[str(game[0][0])]
+        for i in range(game[0][0], game[0][0] + 1 + check(game[1],game[2])):
+            stor[str(i)] = stor.get(str(i),0) + rep
+    return sum([stor[val] for val in stor])
 
-# do part b later 
+print(a())
+print(b())
