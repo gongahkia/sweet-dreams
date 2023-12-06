1307ae591750511f2d4c54132a1c9691b71866ed
Larry <larry.lee.jia.heng@gmail.com>
2023-12-07 01:27:33 +0800

    daygen done

--- Larry/AoC2023/Daygen.py
+++ Larry/AoC2023/Daygen.py
@@ -9,12 +9,10 @@ Year = input("Enter Year (if current year, then leave blank)")
 Day = input("Enter Day to create input for, else leave blank")
 
 Today = datetime.date.today()
-Year = int(str(Today).split("-")[0]) if Year == "" else Year
+Year = str(Today).split("-")[0] if Year == "" else Year
 print(Year)
 Day = int(str(Today).split("-")[2]) if Day == "" else Day
-#print(Day)
-
-Day = 7;print(Day)
+print(Day)
 
 scriptnames = ["AOC", "_input.txt", "_test_1.txt", "_test2.txt",]
 
@@ -22,7 +20,7 @@ dirname = os.path.dirname(__file__)
 
 foldername = dirname + f"\\D{Day}"
 
-print(f"The directory's name is {dirname}")
+# print(f"The directory's name is {dirname}")
 
 if os.path.exists(foldername):
     print("Directory already exists, please make sure day has been completed.")
@@ -34,35 +32,32 @@ os.makedirs(foldername)
 
 #* 1 - create input dump file
 filename = foldername + f"\\AOC{Year[1:]}D{Day}{scriptnames[1]}"
-with open(filename) as f:
+with open(filename,'w') as f:
     f.write("Replace_with_input")
 
 #* 2 - create testcase1 dump file
 filename = foldername + f"\\AOC{Year[1:]}D{Day}{scriptnames[2]}"
-with open(filename) as f:
+with open(filename,'w') as f:
     f.write("Replace_with_testinput1")
 
 #* 3 - create testcase2 dump file
-filename = foldername + f"\\AOC{Year[1:]}D{Day}{scriptnames[2]}"
-with open(filename) as f:
+filename = foldername + f"\\AOC{Year[1:]}D{Day}{scriptnames[3]}"
+with open(filename,'w') as f:
     f.write("Replace_with_testinput2")
 
 
+lazyconvert = f"""import os
 
-f"""
+\'\'\'
 def problem_{Day}_1():
     Paste_qn_statement
-"""
+\'\'\'
+dirname = os.path.dirname(__file__)
 
-filename = "C:\\Users\\ChunChunMaru\\Desktop\\Baby code\\FeetV2\\AoC\\AoC2023\\D6\\AOC23D6_input.txt"
+filename = dirname + "\\AOC\\AOC{Year[1:]}D{Day}{scriptnames[1]}"
 
-test = "C:\\Users\\ChunChunMaru\\Desktop\\Baby code\\FeetV2\\AoC\\AoC2023\\D6\\AOC23D6_test_1.txt"
-test2 = "C:\\Users\\ChunChunMaru\\Desktop\\Baby code\\FeetV2\\AoC\\AoC2023\\D6\\AOC23D6_test_2.txt"
-
-rubbish = f"""\"\"\"
-def problem_6_1():
-    dump_qn_statement_here
-\"\"\"
+test = dirname + "\\AOC{Year[1:]}D{Day}{scriptnames[2]}"
+test2 = dirname + "\\AOC{Year[1:]}D{Day}{scriptnames[3]}"
 
 def process_data(RD):
     # * dump data for processing here
@@ -74,22 +69,59 @@ def problem6_1(filename):
     r.strip()
 
     Rawdata = r.split("\n")
-    # step 1: processing data
     
-    #! swap out with actual data
+    # * processing data 
+    #! swap out with actual var names
     var_name = process_data(Rawdata)
+    
+    #? Dump solution here
     
     return thing
 
-print(wishin_i_was_fishin(test))
-
+print(problem6_1(test))
 """
 
+#* 4 - create problem 1 script file
+filename = foldername + f"\\ZOC{Year}D{Day}_1.py"
+with open(filename,'w') as f:
+    f.write(lazyconvert)
 
+lazyconvert = f"""import os
 
+\'\'\'
+def problem_{Day}_1():
+    Paste_qn_statement
+\'\'\'
+dirname = os.path.dirname(__file__)
 
+filename = dirname + "\\AOC\\AOC{Year[1:]}D{Day}{scriptnames[1]}"
+
+test = dirname + "\\AOC{Year[1:]}D{Day}{scriptnames[2]}"
+test2 = dirname + "\\AOC{Year[1:]}D{Day}{scriptnames[3]}"
+
+def process_data(RD):
+    # * dump data for processing here
+    return relevant_data
+
+def problem6_2(filename):
+    with open(filename) as f:
+        r = f.read()
+    r.strip()
+
+    Rawdata = r.split("\n")
+    
+    # * processing data 
+    #! swap out with actual var names
+    var_name = process_data(Rawdata)
+    
+    #? Dump solution here
+    
+    return thing
+
+print(problem6_2(test2))
+"""
 
 #* 4 - create problem 1 script file
-filename = foldername + f"\\AOC{Year}D{Day}_1.py"
-with open(filename) as f:
+filename = foldername + f"\\ZOC{Year}D{Day}_2.py"
+with open(filename,'w') as f:
     f.write(lazyconvert)
