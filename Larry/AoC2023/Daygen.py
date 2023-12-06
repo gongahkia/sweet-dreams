75c1a028d60d0d9962efb77e9cf1054953eedb51
Larry <larry.lee.jia.heng@gmail.com>
2023-12-07 01:49:04 +0800

    Refactored Daygen.py script

--- Larry/AoC2023/Daygen.py
+++ Larry/AoC2023/Daygen.py
@@ -6,13 +6,12 @@ import datetime
 
 # **Get Year and Date, leave blank if creating for current day
 Year = input("Enter Year (if current year, then leave blank)")
-Day = input("Enter Day to create input for, else leave blank")
+Day =  input("Enter Day  (if current Day,  then leave blank)")
 
 Today = datetime.date.today()
 Year = str(Today).split("-")[0] if Year == "" else Year
-print(Year)
 Day = int(str(Today).split("-")[2]) if Day == "" else Day
-print(Day)
+
 
 scriptnames = ["AOC", "_input.txt", "_test_1.txt", "_test2.txt",]
 
@@ -46,12 +45,13 @@ with open(filename,'w') as f:
     f.write("Replace_with_testinput2")
 
 
-lazyconvert = f"""import os
-
-\'\'\'
+lazyconvert = f"""\'\'\'
 def problem_{Day}_1():
     Paste_qn_statement
 \'\'\'
+
+import os
+
 dirname = os.path.dirname(__file__)
 
 filename = dirname + "\\\\AOC\\\\AOC{Year[1:]}D{Day}{scriptnames[1]}"
@@ -91,12 +91,13 @@ filename = foldername + f"\\ZOC{Year}D{Day}_1.py"
 with open(filename,'w') as f:
     f.write(lazyconvert)
 
-lazyconvert = f"""import os
-
-\'\'\'
+lazyconvert = f"""\'\'\'
 def problem_{Day}_1():
     Paste_qn_statement
 \'\'\'
+
+import os
+
 dirname = os.path.dirname(__file__)
 
 filename = dirname + "\\\\AOC\\\\AOC{Year[1:]}D{Day}{scriptnames[1]}"
